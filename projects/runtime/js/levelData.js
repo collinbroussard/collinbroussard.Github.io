c57be7ba8ff66ea8c4e67aece33bed49fff69f6b
collinbroussard <cb2549997@lpssonline.com>
2023-05-08 13:34:12 +0000

    updated

--- projects/runtime/js/levelData.js
+++ projects/runtime/js/levelData.js
@@ -1,45 +1,178 @@
-var makeLevelData = function (window) {
+var level01 = function (window) {
+
   window.opspark = window.opspark || {};
 
-  window.opspark.makeDataInGame = function (game) {
-    // some useful constants
-    var groundY = game.groundY;
+  var draw = window.opspark.draw;
+  var createjs = window.createjs;
 
-    // this data will allow us to define all of the
-    // behavior of our game
+  window.opspark.runLevelInGame = function(game) {
+      // some useful constants 
+      var groundY = game.groundY;
 
-    // TODO 12: change the below data
-    var levelData = [
-      {
-        name: "Robot Romp",
-        number: 1,
-        speed: -3,
-        gameItems: [
-          { type: "sawblade", x: 400, y: groundY },
-          { type: "sawblade", x: 600, y: groundY },
-          { type: "sawblade", x: 900, y: groundY },
-        ],
-      },
-      {
-        name: "Robot Rampage",
-        number: 2,
-        speed: -3,
-        gameItems: [
-          { type: "sawblade", x: 400, y: groundY },
-          { type: "sawblade", x: 600, y: groundY },
-          { type: "sawblade", x: 900, y: groundY },
-        ],
-      },
-    ];
-    window.opspark.levelData = levelData;
+      // this data will allow us to define all of the
+      // behavior of our game
+      var levelData = [
+          {
+          name: "Robot Romp",
+            number: 1,
+            speed: -3,
+            gameItems: [
+              { type: "sawblade", x: 700, y: groundY-35 },
+            ],
+          },
+          {
+            name: "Robot Rampage",
+            number: 2,
+            speed: -3,
+            gameItems: [
+              { type: "enemy", x: 400, y: groundY-50 },
+              { type: "enemy", x: 600, y: groundY-100 },
+              { type: "reward", x: 900, y: groundY -200 },
+              { type: "reward", "x": 1700, "y": groundY - 60},
+            ],
+          },
+          {
+              name: "Level Awesome",
+              number : 3,
+              speed: -2,
+              gameItems: [
+              { type: "sawblade", x: 400, y: groundY },
+              { type: "enemy", x: 600, y: groundY },
+              { type: "enemy", x: 900, y: groundY },
+              ]
+          }
+      ];
+      window.levelData = levelData;
+      // set this to true or false depending on if you want to see hitzones
+      game.setDebugMode(false);
+
+      // TODO 6 and on go here
+      
+      // BEGIN EDITING YOUR CODE HERE
+      function createSawBlade(x,y) {
+          var hitZoneSize = 25;
+          var damageFromObstacle = 10;
+          var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+          
+          sawBladeHitZone.x = x;
+          sawBladeHitZone.y = y;
+          game.addGameItem(sawBladeHitZone);
+          
+          var obstacleImage = draw.bitmap("img/sawblade.png");
+          obstacleImage.x = -25;
+          obstacleImage.y = -25;
+          sawBladeHitZone.addChild(obstacleImage);
+      }
+      createSawBlade(3279,411)
+      createSawBlade(767,402)
+      createSawBlade(932,432)
+
+      
+      createEnemy = function(x,y) {
+
+          var enemy = game.createGameItem("enemy", 25);
+          var redSquare = draw.rect(50, 50, "red");
+          redSquare.x = -25;
+          redSquare.y = -25;
+          enemy.addChild(redSquare);
+          enemy.x = x;
+          enemy.y = y;
+          game.addGameItem(enemy);
+          enemy.velocityX = - 2;
+
+          
+          enemy.onPlayerCollision = function() {
+          game.changeIntegrity(-10);
+          }
+          enemy.onProjectileCollision = function() {
+              game.increaseScore(100);
+              enemy.shrink()
+              }
+              
+
+          }
+          createEnemy(1550,groundY-50)
+          createEnemy(1300,groundY-75)
+          createEnemy(1288,groundY-90)
+
+
+          function createReward(x,y) {
+
+              var reward = game.createGameItem("reward", 50);
+              var yellowStar = draw.bitmap("img/star-ft_100x100.png");
+              yellowStar.x = -50;
+              yellowStar.y = -50;
+              reward.addChild(yellowStar);
+              
+              reward.x = x;
+              reward.y = y;
+              game.addGameItem(reward);
+              reward.velocityX = -2;
+      
+              reward.onPlayerCollision = function() {
+                  game.changeIntegrity(20)
+              };
+              reward.onProjectileCollision = function () {
+                  game.increaseScore(100);
+                  reward.shrink();
+              }
+            }
+
+            createReward(690,groundY-210)
+            createReward(1450,groundY-70)
+
+          function createMarker(x,y) {
+              var marker = game.createGameItem("marker", 50);
+              var line = draw.bitmap("img/line-symbol_2000x2000.png");
+              line.x = -100;
+              line.y = -100;
+              marker.addChild(line);
+              
+              marker.x = x;
+              marker.y = y;
+              game.addGameItem(marker);
+              marker.velocityX = -3;
+      
+              marker.onPlayerCollision = function() {
+                  game.startLevel()
+              };
+              marker.onProjectileCollision = function () {
+                  game.startLevel()
+                  marker.shrink();
+              }
+            }
+          
+          createMarker(2800,groundY-650)
+
+          for (var i = 0; i < levelData.length; i++) {
+              var level = levelData[i];
+              var gameItems = level.gameItems;
+              for (var j = 0; j < gameItems.length; j++) {
+                  var item = gameItems[j];
+                  var x = item.x;
+                  var y = item.y;
+                  var type = item.type;
+                  if (type === "sawblade") {
+                      createSawBlade(x, y);
+                  } else if (type === "enemy") {
+                      createEnemy(x, y);
+                  } else if (type === "reward") {
+                      createReward(x, y);
+                  } else if (type === "marker") {
+                      createMarker(x, y);
+                  }
+              }
+          }
   };
-};
+  
+     
+      // DO NOT EDIT CODE BELOW HERE
+}
+
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if (
-  typeof process !== "undefined" &&
-  typeof process.versions.node !== "undefined"
-) {
+if((typeof process !== 'undefined') &&
+  (typeof process.versions.node !== 'undefined')) {
   // here, export any references you need for tests //
-  module.exports = makeLevelData;
+  module.exports = level01;
 }
